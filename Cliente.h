15ff95d6f4b26c2ce7fec49daa2cb996efe21dee
Anye-code <anyelacabrera789@gmail.com>
2026-07-06 19:26:02 -0500

    feat: update Cliente.h

---
+++ Cliente.h
@@ -0,0 +1,19 @@
+#ifndef CLIENTE_H
+#define CLIENTE_H
+#include <string>
+
+	class Cliente {
+	private:
+		std::string Identification, Name, Telefono;
+	public:
+		Cliente(std::string auxIdentification, std::string auxName, std::string auxTelefono);
+		std::string getIdentification();
+		std::string getName();
+		std::string getTelefono();
+        void setIdentification(std::string auxIdentification);
+        void setName(std::string auxName);
+		void setTelefono(std::string auxTelefono);
+
+	};
+
+#endif // CLIENTE_H
