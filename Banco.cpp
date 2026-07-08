ee4cd6fa86e2cff40b03b85fa664b7ac6d34e417
CamiloEspinalLeon <camilo.espinal@correounivalle.edu.co>
2026-07-07 21:22:37 -0500

    Add files via upload
    Banc o

--- Banco.cpp
+++ Banco.cpp
@@ -1,27 +1,14 @@
-// Curso: Fundamentos de Programacion Orientada a Objetos
-// Archivo: Banco.cpp
-// Autores:
-// - Juan Ricardo Riaños Horta | Código: 2537573 | juan.rianos@correounivalle.edu.co
-// - Carlos Anibal Ceron del Castillo | Código: 2537097 | carlos.ilich.ceron@correounivalle.edu.co
-// - Anyela Lineth Cabrera Ordoñez | Código: 2540031 | anyela.cabrera@correounivalle.edu.co
-// - Camilo Espinal León | Código: 2538740 | camilo.espinal@correounivalle.edu.co
-// - Juan José Peña Garcés | Código: 2538880 | juan.jose.pena@correounivalle.edu.co
-// Fecha: 06/07/2026
-
-#include <iostream>
 #include "Banco.h"
-#include <string>
-// Constructor: 
-Banco::Banco(std::string auxNombre) {
-    Nombre = auxNombre;
+#include <iostream>
+
+Banco::Banco(std::string nombre) {
+    Nombre = nombre;
 }
 
-// Getters
 std::string Banco::getNombre() {
     return Nombre;
 }
 
-// Setters
 void Banco::addTitularidad(Titularidad t) {
     titulares.push_back(t);
 }
@@ -31,10 +18,29 @@ void Banco::addTitularidadReferencia(Titularidad& t) {
 }
 
 void Banco::imprimirDatos() {
-    std::cout << "Nombre del banco: " << Nombre << std::endl;
-    std::cout << "Titulares del banco: " << std::endl;
-    for (int i = 0; i < titulares.size(); ++i) {
-        Titularidad titular = titulares[i];
-        std::cout << "Tipo de titular: " << titular.getTipoTitular() << ", Fecha de vinculación: " << titular.getFechaVinculacion() << std::endl;
+    cout << "Banco: " << Nombre << endl;
+    cout << "Numero de titularidades registradas: " << titulares.size() << endl;
+    for (size_t i = 0; i < titulares.size(); i++) {
+        Cliente* c = titulares[i].getCliente();
+        Cuenta* cu = titulares[i].getCuenta();
+        cout << "-----------------------------------" << endl;
+        cout << "Cliente: " << c->getNombre() << " (ID: " << c->getIdentificacion() << ")" << endl;
+        cout << "Telefono: " << c->getTelefono() << endl;
+        cout << "Cuenta N. " << cu->getNumeroCuenta() << " | Saldo: " << cu->getSaldo() << endl;
+        cout << "Tipo de titular: " << titulares[i].getTipoTitular() << endl;
+        cout << "Fecha de vinculacion: " << titulares[i].getFechaVinculacion() << endl;
     }
+}
+
+void Banco::calcularPromedio() {
+    if (titulares.empty()) {
+        cout << "No hay cuentas registradas para calcular el promedio." << endl;
+        return;
+    }
+    long total = 0;
+    for (size_t i = 0; i < titulares.size(); i++) {
+        total += titulares[i].getCuenta()->getSaldo();
+    }
+    double promedio = (double)total / titulares.size();
+    cout << "Saldo promedio de las cuentas del banco: " << promedio << endl;
 }
